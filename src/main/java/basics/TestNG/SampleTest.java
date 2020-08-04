22605785cdd3a20f64c7607662408be307a53b93
Khanh Do <khanhdo.pmp@gmail.com>
2020-08-04 16:31:26 +0700

    Add test case TestNG #1

---
+++ src/main/java/basics/TestNG/SampleTest.java
@@ -0,0 +1,18 @@
+package basics.TestNG;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+
+    @Test
+    public void test1() // First test case.
+    {
+        System.out.println("test1");
+    }
+
+    @Test
+    public void test2() // Second test case.
+    {
+        System.out.println("test2");
+    }
+}
